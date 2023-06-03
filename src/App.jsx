b02a860ec23dc406b22a7faa9e6db4a97f50c1eb
jona <jona._09@hotmail.com>
2023-06-02 23:28:42 -0300

    se inicializa proyecto con js scss y js

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "../components/Navbar";
+import ButtonAcordeon from '../components/Button-acordeon'
+
+function App() {
+  return (
+    <>
+      <Home />
+      <ButtonAcordeon />
+
+    </>
+  );
+}
+
+export default App;
