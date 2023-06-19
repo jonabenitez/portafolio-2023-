3d9d77dfaf2b613056fbc8dc80a030f84987396b
jona <jona._09@hotmail.com>
2023-06-19 14:01:05 -0300

    se agrega lista de tecnologias

--- my-components/Intro-section.jsx
+++ my-components/Intro-section.jsx
@@ -8,7 +8,14 @@ import CodingFlatline from "../img/arts/Coding _Monochromatic.png";
 //variables icons
 const iconsCall = <FontAwesomeIcon icon={faPhoneVolume} />;
 const iconsPlay = <FontAwesomeIcon icon={faCirclePlay} />;
-const wave = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" d="M0,288L40,293.3C80,299,160,309,240,282.7C320,256,400,192,480,170.7C560,149,640,171,720,197.3C800,224,880,256,960,250.7C1040,245,1120,203,1200,192C1280,181,1360,203,1400,213.3L1440,224L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+const wave = (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <path
+      fill="#ffffff"
+      d="M0,288L40,293.3C80,299,160,309,240,282.7C320,256,400,192,480,170.7C560,149,640,171,720,197.3C800,224,880,256,960,250.7C1040,245,1120,203,1200,192C1280,181,1360,203,1400,213.3L1440,224L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+    ></path>
+  </svg>
+);
 //intro section
 
 function Section1() {
@@ -39,20 +46,24 @@ function Section1() {
           </div>
           <div className="col-md-6 intros text-end">
             <div className="video-box ">
-              <img src={CodingFlatline} alt="null" className="img-fluid" />
-              <a href="#" className="position-absolute top-50 start-50 translate-middle">  
+              <img src={CodingFlatline} alt="null" className="w-100 h-100" />
+              <a
+                href="#"
+                className="position-absolute top-50 start-50 translate-middle"
+              >
                 <span>
                   <i>{iconsPlay}</i>
                 </span>
                 <span className="border-animation border-animation--border-1"></span>
                 <span className="border-animation border-animation--border-2"></span>
-
               </a>
             </div>
           </div>
         </div>
       </div>
+      <div>
       {wave}
+      </div>
     </section>
   );
 }
--- my-components/Tecnologies.jsx
+++ my-components/Tecnologies.jsx
@@ -7,25 +7,53 @@ function Tecnologies() {
       {/**TITTLE CONTENT */}
       <div className="container">
         <div className="text-center">
-          <h4 className="fw-bold lead mb-2">Tecnologias hasta el momento</h4>
+          <h4 className="fw-bold display-6 mb-2">Tecnologias hasta el momento</h4>
           <div className="heading-line"></div>
         </div>
       </div>
 
       {/**TECNOLOGIES CONTENT */}
       <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <div className="campanies__logo-box d-flex">
+        <div className="row pt-5">
+          <div className="col-md-3">
+            <div className="campanies__logo-box shadow-sm">
               <img src={BaseImg[0].img} alt="" />
+            </div>
+         </div>
+         <div className="col-md-3">
+            <div className="campanies__logo-box shadow-sm">
               <img src={BaseImg[1].img} alt="" />
+            </div>
+         </div>
+         <div className="col-md-3">
+            <div className="campanies__logo-box shadow-sm">
               <img src={BaseImg[2].img} alt="" />
+            </div>
+         </div>
+
+         <div className="col-md-3">
+            <div className="campanies__logo-box shadow-sm">
               <img src={BaseImg[3].img} alt="" />
+            </div>
+         </div>
+
+         <div className="col-md-3">
+            <div className="campanies__logo-box shadow-sm">
               <img src={BaseImg[4].img} alt="" />
+            </div>
+         </div>
+         <div className="col-md-3">
+            <div className="campanies__logo-box shadow-sm">
               <img src={BaseImg[5].img} alt="" />
+            </div>
+         </div>
+
+         <div className="col-md-3">
+            <div className="campanies__logo-box shadow-sm">
               <img src={BaseImg[6].img} alt="" />
             </div>
-          </div>
+         </div>
+
         </div>
       </div>
     </section>
