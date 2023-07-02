5780693d9508b0548dac6073f5d9c9277752eb25
jona <jona._09@hotmail.com>
2023-07-02 18:03:32 -0300

    se avanza con section myproyects con tipografia y diseno

--- my-components/MyProyects.jsx
+++ my-components/MyProyects.jsx
@@ -3,11 +3,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { faListCheck } from "@fortawesome/free-solid-svg-icons";
 import { faTableCellsLarge } from "@fortawesome/free-solid-svg-icons";
+import Button2 from "../my-components/Button2";
+
 import WebDevelopmentImg from "../img/arts/WebDevelopment.png";
 import TodolistImg from "../img/arts/Checklist.png";
 import ColorGameImg from "../img/arts/ColorGame.png";
-
-import Button2 from "../my-components/Button2";
 
 const TravelIcons = <FontAwesomeIcon icon={faPaperPlane} />;
 const TodoList = <FontAwesomeIcon icon={faListCheck} />;
@@ -15,84 +15,94 @@ const ColorGame = <FontAwesomeIcon icon={faTableCellsLarge} />;
 
 function MyProyects() {
   return (
-    //START PROYECTS CONTENT SECTIO 1
+    <section id="proyects" className="services">
+      <div className="row text-center">
+        <h3 className="display-3 fw-bold">my Proyects</h3>
+        <div className="heading-line"></div>
+      </div>
 
-    <div className="container">
+      {/**section 1- websitex */}
+      <div className="container">
       <div className="row">
-        <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 mt-4 services mt-4">
-          <div className="services__content mt-5 pt-5">
-            <span className="display-2">{TravelIcons}</span>
-            <h3 className="mt-4">Proyect Web Travel</h3>
-            <p className="lh-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-              repudiandae, voluptas possimus iure excepturi commodi officiis,
-              dolore alias ipsam corrupti aut, autem in illum accusamus labore
-              nisi reiciendis ex deleniti.
+        <div className="col-lg-6 col-md-6 col-sm-12 col-cs-12 services text-start">
+          <div className="services__content">
+            <div className="icon">{TravelIcons}</div>
+            <h3 className="display-3--title mt-1">web development</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic
+              cumque ducimus corporis assumenda excepturi harum dolor
+              perspiciatis odit iusto ut. Eos perspiciatis obcaecati quia! Ad ab
+              beatae adipisci sed soluta?
             </p>
-            <div className="mt-2">
+            <div className="">
               <Button2 />
             </div>
           </div>
         </div>
 
-        <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 mt-4 services mt-4">
-          <div className="services__content">
-            <img className="" src={WebDevelopmentImg} alt="" />
+        <div className="col-lg-6 col-md-6 col-sm-12 col-cs-12 services text-end">
+          <div className="services__pic">
+            <img
+              className="img-fluid"
+              src={WebDevelopmentImg}
+              alt="wedeveloper"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/**section 2 */}
+
+      <div className="row">
+        <div className="col-lg-6 col-md-6 col-sm-12 col-cs-12 services text-start">
+          <div className="services__pic">
+            <img className="img-fluid" src={TodolistImg} alt="TODOLIST" />
           </div>
         </div>
 
-        {/**SECTION 2 */}
-
-        <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 mt-4 services mt-4">
+        <div className="col-lg-6 col-md-6 col-sm-12 col-cs-12 services">
           <div className="services__content">
-            <img src={TodolistImg} alt="" />
-          </div>
-        </div>
-
-        <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 mt-4 services mt-4">
-          <div className="services__content">
-            <span className="display-2">{TodoList}</span>
-
-            <h3>TodoList React</h3>
-            <p className="lh-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-              repudiandae, voluptas possimus iure excepturi commodi officiis,
-              dolore alias ipsam corrupti aut, autem in illum accusamus labore
-              nisi reiciendis ex deleniti.
+            <div  className="icon">{TodoList}</div>
+            <h3 className="display-3--title mt-1">TodoList React</h3>
+            <p className="">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic
+              cumque ducimus corporis assumenda excepturi harum dolor
+              perspiciatis odit iusto ut. Eos perspiciatis obcaecati quia! Ad ab
+              beatae adipisci sed soluta?
             </p>
-            <div>
+            <div className="">
+              <Button2 />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/**section 3 */}
+      <div className="row">
+        <div className="col-lg-6 col-md-6 col-sm-12 col-cs-12 services">
+          <div className="services__content">
+            <div  className="icon">{ColorGame}</div>
+            <h3 className="display-3--title mt-1">Color Game Js</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic
+              cumque ducimus corporis assumenda excepturi harum dolor
+              perspiciatis odit iusto ut. Eos perspiciatis obcaecati quia! Ad ab
+              beatae adipisci sed soluta?
+            </p>
+            <div className="">
               <Button2 />
             </div>
           </div>
         </div>
 
-        {/**SECTION 3 */}
-        <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 mt-4 services">
-          <div className="services__content">
-            <span className="display-2">{ColorGame}</span>
-
-            <h3>ColorGame JavaScript</h3>
-            <p className="lh-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-              repudiandae, voluptas possimus iure excepturi commodi officiis,
-              dolore alias ipsam corrupti aut, autem in illum accusamus labore
-              nisi reiciendis ex deleniti.
-            </p>
-            <div>
-              <Button2 />
-            </div>
-          </div>
-        </div>
-
-        <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 mt-4 services">
-          <div className="services__content">
-            <img src={ColorGameImg} alt="" />
+        <div className="col-lg-6 col-md-6 col-sm-12 col-cs-12 services text-end">
+          <div className="services__pic">
+            <img className="img-fluid" src={ColorGameImg} alt="colorGame" />
           </div>
         </div>
       </div>
     </div>
-
+    </section>
   );
 }
-
 export default MyProyects;
