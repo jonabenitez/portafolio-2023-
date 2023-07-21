1df18efbaf5bfc304c50e9caec57b99bf1aa7391
jona <jona._09@hotmail.com>
2023-07-21 12:36:42 -0300

    se finaliza con formulario getstarted

--- my-components/GetStarted.jsx
+++ my-components/GetStarted.jsx
@@ -1,3 +1,8 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+
+const checkList = <FontAwesomeIcon icon={faCheck} />;
+const paperPlane = <FontAwesomeIcon icon={faPaperPlane} />;
 function GetStarted() {
   return (
     <section className="get-started">
@@ -10,27 +15,90 @@ function GetStarted() {
             deleniti, aliquid quae itaque
           </p>
         </div>
-      </div>
 
-      {/**SECTION INBOX*/}
-      <div className="row">
-        <div className="col-12 col-lg-6 gradient shadow p-3 text-white">
-          <div className="cta-info w-100">
-            <h4 className="display-4 fw-bold">100% Satisfaction guaranteed</h4>
-            <p className="lhlg">
-              {" "}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-              deserunt delectus volupta
-            </p>
-            <h3 className="display-3--brief">What will the next step?</h3>
-            <ul className="cta-info__list">
-              <li>lorem ipsum</li>
-              <li>lorem ipsum</li>
-              <li>lorem ipsum</li>
-            </ul>
+        {/**SECTION INBOX*/}
+        <div className="row text-white ">
+          <div className="col-12 col-lg-6 gradient shadow p-3">
+            <div className="cta-info w-100">
+              <h4 className="display-4 fw-bold">
+                100% Satisfaction guaranteed
+              </h4>
+              <p className="lh-lg">
+                {" "}
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident deserunt delectus volupta um dolor sit amet
+                consectetur adipisicing elit. Provident deserunt delectus volupt
+              </p>
+              <h3 className="display-3--brief">What will the next step?</h3>
+              <ul className="cta-info__list">
+                <li>
+                  {" "}
+                  <span>{checkList}</span> lorem ipsum
+                </li>
+                <li>
+                  {" "}
+                  <span>{checkList}</span> lorem ipsum
+                </li>
+                <li>
+                  {" "}
+                  <span>{checkList}</span> lorem ipsum
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/**SECOND COLUM - FORM */}
+          <div className="col-12 col-lg-6 bg-white shadow p-3">
+            <div className="form w-100 pb-2">
+              <h4 className="display-3--title mb-5">Start Your Project</h4>
+              <form action="#" className="row ">
+                <div className="col-lg-6 mb-3">
+                  <input
+                    type="text"
+                    placeholder="FirsName"
+                    id="inputFirsName"
+                    className="form-control form-control-lg"
+                  />
+                </div>
+                <div className="col-lg-6 mb-3 ">
+                  <input
+                    type="text"
+                    placeholder="LastName"
+                    id="inputLastName"
+                    className="form-control form-control-lg"
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    placeholder="email"
+                    id="inpuEmail"
+                    className="form-control form-control-lg"
+                  />
+                </div>
+                <div className="mb-3">
+                  <textarea
+                    placeholder="message"
+                    name="Message"
+                    id="message"
+                    cols="30"
+                    rows="8"
+                    className="form-control"
+                  ></textarea>
+                </div>
+                <div className="text-center d-grid mt-1">
+                  <button
+                    type="button"
+                    className="btn btn-primary rounded-pill pb-3"
+                  >
+                    submit
+                    <i className="p-2">{paperPlane}</i>
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-        <div className="col-12 col-lg-6 bg-white shadow p-3">asd</div>
       </div>
     </section>
   );
